200bca3b37d25b5bc4809eaa495512538a2ad925
Kostya Mestukov <kostyamestukov784@gmail.com>
2025-09-17 15:35:10 +0300

    ÐВтоFirst stage: done

--- first_version.py
+++ first_version.py
@@ -14,16 +14,7 @@ def act():
     else:
         return f"{b[0]}: command not found"
 
-# if __name__ == '__main__':
-#     args = sys.argv
-#
-#     path = args[1]
-#     prompt = args[2]
-#     script_path = args[3]
-#
-#     while True:
-#         a = input(f"{prompt}> ")
-#         print(act(a))
+
 
 while True:
     print(act())
