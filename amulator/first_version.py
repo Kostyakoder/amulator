9e05a9e79d98ba0ba76f253e81acbcce3e525619
Kostya Mestukov <kostyamestukov784@gmail.com>
2025-09-17 15:04:14 +0300

    first commit

---
+++ amulator/first_version.py
@@ -0,0 +1,36 @@
+import os
+
+
+def act(a):
+    b = a.split()
+    if a == 'exit':
+        exit()
+    if (len(b)) == 0:
+        return ""
+    if b[0] == "ls":
+        return b
+    elif b[0] == 'cd':
+        return b
+    elif b[0] == 'pwd':
+        return b
+    else:
+        return f"{b[0]}: command not found"
+
+
+
+if __name__ == "__main__":
+    prompt = "$~ kostyaKoder@mireahost:~$"
+    script_path = "commands.txt"
+
+    with open(script_path, 'r') as file:
+        commands = file.readlines()
+        for cmd in commands:
+            cmd = cmd.strip()
+            if cmd:
+                print(f"{os.getcwd()} {prompt}> {cmd}")
+                print(act(cmd))
+                if (act(cmd) == f"{cmd}: command not found"):
+                    break
+    # while True:
+    #     a = input(f"{prompt}> ")
+    #     print(act(a))
